f887c4e5ca8f1a9fccff72bdbec7fb25970ea18c
Jeremy Alexander <jdoubleua@gmail.com>
2015-04-18 02:29:28 -0400

    Added collections and views

---
+++ js/collections/fireplaceCollection.js
@@ -0,0 +1,5 @@
+var app = app || {};
+
+app.fireplaceCollection = Backbone.Collection.extend({
+    model: app.fireplace
+});
---
+++ js/views/fireplaceView.js
@@ -0,0 +1,5 @@
+var app = app || {};
+
+app.fireplaceView = Backbone.View.extend({
+
+});
